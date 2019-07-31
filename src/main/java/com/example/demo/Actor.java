cde91927d1d20b8a13e87400e093df706e12af33
fdmckie <fdmckie@gmail.com>
2019-07-31 18:50:58 -0400

    Example 503 Cloudinary

--- src/main/java/com/example/demo/Actor.java
+++ src/main/java/com/example/demo/Actor.java
@@ -6,6 +6,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+//Create Actor class
 public class Actor {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
